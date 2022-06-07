bd8c7185f6ac07b1b15051f929d62850e285c9c8
anilbarad <anilbarad9@gmail.com>
2022-06-07 17:08:06 +0530

    changed handlerequest url

--- mobiles/views.py
+++ mobiles/views.py
@@ -496,7 +496,7 @@ def profileView(request):
             'INDUSTRY_TYPE_ID': 'Retail',
             'WEBSITE': 'WEBSTAGING',
             'CHANNEL_ID': 'WEB',
-            'CALLBACK_URL': 'http://127.0.0.1:8000/handlerequest/',
+            'CALLBACK_URL': 'http://eget24.herokuapp.com/handlerequest/',
         }
         param_dict['CHECKSUMHASH'] = Checksum.generate_checksum(
             param_dict, MERCHANT_KEY)
